447629e6c892de5e377025106d031bdc86639585
fogmart <aifogmart@mail.ru>
2019-10-13 10:13:53 +1000

    init

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://op.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://op.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/proj/opencart2/catalog/');
+define('DIR_SYSTEM', 'D:/proj/opencart2/system/');
+define('DIR_IMAGE', 'D:/proj/opencart2/image/');
+define('DIR_LANGUAGE', 'D:/proj/opencart2/catalog/language/');
+define('DIR_TEMPLATE', 'D:/proj/opencart2/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/proj/opencart2/system/config/');
+define('DIR_CACHE', 'D:/proj/opencart2/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/proj/opencart2/system/storage/download/');
+define('DIR_LOGS', 'D:/proj/opencart2/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/proj/opencart2/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/proj/opencart2/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'op');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
